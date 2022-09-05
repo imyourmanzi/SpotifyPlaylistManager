2f43f7d8533496f9dfa7d17b0519427c97903367
Matt Manzi <imyourmanzi1@gmail.com>
2022-09-05 12:15:14 -0400

    Add error and loading handlers to `Me` component

--- packages/ui/src/app/components/me/Me.tsx
+++ packages/ui/src/app/components/me/Me.tsx
@@ -9,6 +9,17 @@ import { LabelSmall, MonoLabelSmall, ParagraphSmall } from 'baseui/typography';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSpotifyAuth } from '@contexts/spotify-auth/SpotifyAuth';
+import { toaster, ToasterContainer } from 'baseui/toast';
+
+/**
+ * A property for each request type this component makes, allowing storing state to be
+ * more concise and consistent for all request (e.g. for loading or error states).
+ */
+type RequestTypes = {
+  me: boolean;
+  newToken: boolean;
+  playlists: boolean;
+};
 
 export const Me = () => {
   const {
@@ -16,25 +27,63 @@ export const Me = () => {
     setAccessToken,
     setRefreshToken,
   } = useSpotifyAuth();
+
+  const [loadingStates, setLoadingStates] = useState<RequestTypes>({
+    me: false,
+    newToken: false,
+    playlists: false,
+  });
+
+  const [errors, setErrors] = useState<RequestTypes>({
+    me: false,
+    newToken: false,
+    playlists: false,
+  });
+
   const [userData, setUserData] = useState<Record<string, any> | null>(null);
-  const [loadingNewToken, setLoadingNewToken] = useState(false);
   const [playlists, setPlaylists] = useState<
     { id: string; name: string; tracks: { total: number } }[]
   >([]);
 
   const [css, theme] = useStyletron();
 
-  useEffect(() => {
-    if (accessToken) {
+  useEffect(
+    () => {
+      if (!accessToken || errors.me) {
+        return;
+      }
+
+      setLoadingStates({ ...loadingStates, me: true });
       fetch('https://api.spotify.com/v1/me', {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
       })
-        .then((response) => response.json())
-        .then(setUserData);
-    }
-  }, []);
+        .then(async (response) => {
+          const data = await response.json();
+
+          if (data.error) {
+            setErrors({ ...errors, me: true });
+            toaster.negative(
+              'Unable to fetch user data, please refresh or log out and back in again',
+              {
+                autoHideDuration: 4000,
+                onClose: () => {
+                  setErrors({ ...errors, me: false });
+                },
+              },
+            );
+            return;
+          }
+
+          setUserData(data);
+        })
+        .finally(() => {
+          setLoadingStates({ ...loadingStates, me: false });
+        });
+    },
+    [accessToken], // purposefully leaving out `errors` so we don't constantly retry when the toast closes
+  );
 
   const navigate = useNavigate();
 
@@ -48,39 +97,78 @@ export const Me = () => {
   };
 
   const fetchNewToken = () => {
-    setLoadingNewToken(true);
+    if (!refreshToken || errors.newToken) {
+      return;
+    }
+
+    setLoadingStates({ ...loadingStates, newToken: true });
     fetch(
       `/api/refresh_token?${new URLSearchParams({
         refresh_token: refreshToken,
       }).toString()}`,
     )
       .then(async (response) => {
-        const { access_token } = await response.json();
-        setAccessToken(access_token);
+        const { access_token, error } = await response.json();
+
+        if (error) {
+          setErrors({ ...errors, newToken: true });
+          toaster.negative(
+            'Unable to fetch new token, please refresh or log out and back in again',
+            {
+              autoHideDuration: 4000,
+              onClose: () => {
+                setErrors({ ...errors, newToken: false });
+              },
+            },
+          );
+          return;
+        }
+
+        setAccessToken(access_token as string);
       })
       .finally(() => {
-        setLoadingNewToken(false);
+        setLoadingStates({ ...loadingStates, newToken: false });
       });
   };
 
   const getPlaylists = () => {
-    if (!userData?.id) {
+    if (!accessToken || !userData?.id || errors.playlists) {
       return;
     }
 
+    setLoadingStates({ ...loadingStates, playlists: true });
     fetch(`https://api.spotify.com/v1/users/${userData.id}/playlists`, {
       headers: {
         Authorization: 'Bearer ' + accessToken,
       },
-    }).then(async (response) => {
-      const data = await response.json();
-      setPlaylists(data.items);
-    });
+    })
+      .then(async (response) => {
+        const data = await response.json();
+
+        if (data.error) {
+          setErrors({ ...errors, playlists: true });
+          toaster.negative(
+            'Unable to playlists, please refresh or log out and back in again',
+            {
+              autoHideDuration: 4000,
+              onClose: () => {
+                setErrors({ ...errors, playlists: false });
+              },
+            },
+          );
+          return;
+        }
+
+        setPlaylists(data.items);
+      })
+      .finally(() => {
+        setLoadingStates({ ...loadingStates, playlists: false });
+      });
   };
 
   return (
     <>
-      {userData && (
+      <ToasterContainer>
         <div className={css({ width: '70%', margin: 'auto 15% 5%', textAlign: 'left' })}>
           <HeadingLevel>
             <FlexGrid flexGridColumnCount={2} alignItems={'baseline'}>
@@ -89,7 +177,10 @@ export const Me = () => {
               </FlexGridItem>
               <FlexGridItem>
                 <MonoLabelSmall className={css({ textAlign: 'right' })}>
-                  logged in as {userData.display_name} /{' '}
+                  {userData?.error
+                    ? `logged in as ${userData.display_name}`
+                    : 'data error'}{' '}
+                  /{' '}
                   <StyledLink
                     className={css({ ...theme.typography.MonoLabelSmall })}
                     href="/"
@@ -100,71 +191,81 @@ export const Me = () => {
                 </MonoLabelSmall>
               </FlexGridItem>
             </FlexGrid>
-            <Avatar size="scale4800" src={userData.images['0'].url} />
-            <LabelSmall>Display Name</LabelSmall>
-            <ParagraphSmall>{userData.display_name}</ParagraphSmall>
-            <LabelSmall>ID</LabelSmall>
-            <ParagraphSmall>{userData.id}</ParagraphSmall>
-            <LabelSmall>Email</LabelSmall>
-            <ParagraphSmall>{userData.email}</ParagraphSmall>
-            <LabelSmall>Spotify URI</LabelSmall>
-            <ParagraphSmall>
-              <StyledLink href={userData.external_urls.spotify}>
-                {userData.external_urls.spotify}
-              </StyledLink>
-            </ParagraphSmall>
-            <LabelSmall>API Link</LabelSmall>
-            <ParagraphSmall>
-              <StyledLink href={userData.href}>{userData.href}</StyledLink>
-            </ParagraphSmall>
-            <LabelSmall>Profile Image</LabelSmall>
-            <ParagraphSmall>
-              <StyledLink href={userData.images['0'].url}>
-                {userData.images['0'].url}
-              </StyledLink>
-            </ParagraphSmall>
-            <LabelSmall>Country</LabelSmall>
-            <ParagraphSmall>{userData.country}</ParagraphSmall>
-            <HeadingLevel>
-              <Heading>OAuth Details</Heading>
-              <LabelSmall>Access token</LabelSmall>
-              <ParagraphSmall>{accessToken}</ParagraphSmall>
-              <LabelSmall>Refresh token</LabelSmall>
-              <ParagraphSmall>{refreshToken}</ParagraphSmall>
-              <Button
-                size="mini"
-                disabled={!refreshToken}
-                isLoading={loadingNewToken}
-                onClick={fetchNewToken}
-              >
-                Obtain new token using the refresh token
-              </Button>
-              <Heading>More Actions</Heading>
-              <FlexGrid flexGridColumnCount={4}>
-                <FlexGridItem>
-                  <Button disabled={!userData?.id} onClick={getPlaylists}>
-                    List some playlists
-                  </Button>
-                </FlexGridItem>
-              </FlexGrid>
-              {!!playlists.length && (
+            {!loadingStates.me && !errors.me && userData && (
+              <>
+                <Avatar size="scale4800" src={userData.images['0'].url} />
+                <LabelSmall>Display Name</LabelSmall>
+                <ParagraphSmall>{userData.display_name}</ParagraphSmall>
+                <LabelSmall>ID</LabelSmall>
+                <ParagraphSmall>{userData.id}</ParagraphSmall>
+                <LabelSmall>Email</LabelSmall>
+                <ParagraphSmall>{userData.email}</ParagraphSmall>
+                <LabelSmall>Spotify URI</LabelSmall>
+                <ParagraphSmall>
+                  <StyledLink href={userData.external_urls.spotify}>
+                    {userData.external_urls.spotify}
+                  </StyledLink>
+                </ParagraphSmall>
+                <LabelSmall>API Link</LabelSmall>
+                <ParagraphSmall>
+                  <StyledLink href={userData.href}>{userData.href}</StyledLink>
+                </ParagraphSmall>
+                <LabelSmall>Profile Image</LabelSmall>
+                <ParagraphSmall>
+                  <StyledLink href={userData.images['0'].url}>
+                    {userData.images['0'].url}
+                  </StyledLink>
+                </ParagraphSmall>
+                <LabelSmall>Country</LabelSmall>
+                <ParagraphSmall>{userData.country}</ParagraphSmall>
                 <HeadingLevel>
-                  <Heading>Playlists</Heading>
-                  <ul>
-                    {playlists.map((playlist) => (
-                      <ListItem key={playlist.id}>
-                        <ListItemLabel description={`${playlist.tracks.total} tracks`}>
-                          {playlist.name}
-                        </ListItemLabel>
-                      </ListItem>
-                    ))}
-                  </ul>
+                  <Heading>OAuth Details</Heading>
+                  <LabelSmall>Access token</LabelSmall>
+                  <ParagraphSmall>{accessToken}</ParagraphSmall>
+                  <LabelSmall>Refresh token</LabelSmall>
+                  <ParagraphSmall>{refreshToken}</ParagraphSmall>
+                  <Button
+                    size="mini"
+                    disabled={!refreshToken}
+                    isLoading={loadingStates.newToken}
+                    onClick={fetchNewToken}
+                  >
+                    Obtain new token using the refresh token
+                  </Button>
+                  <Heading>More Actions</Heading>
+                  <FlexGrid flexGridColumnCount={4}>
+                    <FlexGridItem>
+                      <Button
+                        disabled={!userData?.id}
+                        isLoading={loadingStates.playlists}
+                        onClick={getPlaylists}
+                      >
+                        List some playlists
+                      </Button>
+                    </FlexGridItem>
+                  </FlexGrid>
+                  {!!playlists.length && (
+                    <HeadingLevel>
+                      <Heading>Playlists</Heading>
+                      <ul>
+                        {playlists.map((playlist) => (
+                          <ListItem key={playlist.id}>
+                            <ListItemLabel
+                              description={`${playlist.tracks.total} tracks`}
+                            >
+                              {playlist.name}
+                            </ListItemLabel>
+                          </ListItem>
+                        ))}
+                      </ul>
+                    </HeadingLevel>
+                  )}
                 </HeadingLevel>
-              )}
-            </HeadingLevel>
+              </>
+            )}
           </HeadingLevel>
         </div>
-      )}
+      </ToasterContainer>
     </>
   );
 };
