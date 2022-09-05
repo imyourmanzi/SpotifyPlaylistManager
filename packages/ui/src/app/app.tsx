81c5cc196fd4f8608c3cd3fe8f4b1ddcf7a16cce
Matt Manzi <imyourmanzi1@gmail.com>
2022-09-05 11:29:55 -0400

    Enable UI to inherit light/dark mode from system

--- packages/ui/src/app/app.tsx
+++ packages/ui/src/app/app.tsx
@@ -1,33 +1,27 @@
-import { useStyletron } from 'baseui';
 import { StyledDivider } from 'baseui/divider';
 import { DisplayMedium } from 'baseui/typography';
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { Home } from '@components/home/Home';
 import { Me } from '@components/me/Me';
 import { SpotifyLoginCallbackHandler } from '@components/spotify-login-callback-handler/SpotifyLoginCallbackHandler';
-import { SpotifyAuthProvider } from '@contexts/spotify-auth/SpotifyAuth';
+import { useStyletron } from 'baseui';
 
 export function App() {
   const [css] = useStyletron();
   const navigate = useNavigate();
 
   return (
-    <SpotifyAuthProvider>
-      <div className={css({ textAlign: 'center', paddingTop: '3rem' })}>
-        <DisplayMedium
-          className={css({ cursor: 'pointer' })}
-          onClick={() => navigate('/')}
-        >
-          Spotify Playlist Manager
-        </DisplayMedium>
-        <StyledDivider className={css({ margin: '2rem' })} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/callback" element={<SpotifyLoginCallbackHandler />} />
-          <Route path="/me" element={<Me />} />
-        </Routes>
-      </div>
-    </SpotifyAuthProvider>
+    <div className={css({ textAlign: 'center', paddingTop: '3rem' })}>
+      <DisplayMedium className={css({ cursor: 'pointer' })} onClick={() => navigate('/')}>
+        Spotify Playlist Manager
+      </DisplayMedium>
+      <StyledDivider className={css({ margin: '2rem' })} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/callback" element={<SpotifyLoginCallbackHandler />} />
+        <Route path="/me" element={<Me />} />
+      </Routes>
+    </div>
   );
 }
 
--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,26 +1,58 @@
-import { DarkTheme, BaseProvider } from 'baseui';
-import { StrictMode } from 'react';
+import { DarkTheme, BaseProvider, LightTheme } from 'baseui';
+import { StrictMode, useCallback, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Client as Styletron } from 'styletron-engine-atomic';
 import { Provider as StyletronProvider } from 'styletron-react';
+import { SpotifyAuthProvider } from '@contexts/spotify-auth/SpotifyAuth';
 import App from './app/app';
 
 const engine = new Styletron();
 
 const rootElement = document.getElementById('root')!;
-rootElement.parentElement?.setAttribute('style', 'background-color: rgb(0, 0, 0)');
+
+const getLightThemeQuery = () => window.matchMedia('(prefers-color-scheme: light)');
+
+const setBodyBackground = (light: boolean) => {
+  (rootElement.parentElement as HTMLBodyElement).style.backgroundColor = light
+    ? 'rgb(245,247,250)'
+    : 'rgb(0,0,27)';
+};
+
+const AppWithProviders = () => {
+  const [useLightTheme, setUseLightTheme] = useState(getLightThemeQuery().matches);
+  setBodyBackground(useLightTheme);
+
+  const updateTheme = useCallback((event: MediaQueryListEvent) => {
+    const requestedLightTheme = event.matches;
+    setUseLightTheme(requestedLightTheme);
+  }, []);
+
+  useEffect(() => {
+    const mediaQuery = getLightThemeQuery();
+    mediaQuery.addEventListener('change', updateTheme);
+
+    // this is the cleanup function to remove the listener
+    return () => mediaQuery.removeEventListener('change', updateTheme);
+  }, []);
+
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={useLightTheme ? LightTheme : DarkTheme}>
+        <SpotifyAuthProvider>
+          <App />
+        </SpotifyAuthProvider>
+      </BaseProvider>
+    </StyletronProvider>
+  );
+};
 
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <StyletronProvider value={engine}>
-        <BaseProvider theme={DarkTheme}>
-          <App />
-        </BaseProvider>
-      </StyletronProvider>
+      <AppWithProviders />
     </BrowserRouter>
   </StrictMode>,
 );
