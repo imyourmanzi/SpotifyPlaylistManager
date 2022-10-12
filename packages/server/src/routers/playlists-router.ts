d08f84d684a85bc945a271d4deefced5d34c2f3c
Matt Manzi <imyourmanzi1@gmail.com>
2022-10-11 21:01:44 -0400

    Finish logic to hydrate playlists (if owned)
    and supports non-owned playlists without track lists

--- packages/server/src/routers/playlists-router.ts
+++ packages/server/src/routers/playlists-router.ts
@@ -1,14 +1,15 @@
 import type { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { Static, Type } from '@sinclair/typebox';
-import { URLSearchParams } from 'node:url';
+import type { AxiosInstance } from 'axios';
 import {
   GetMeResponseType,
   GetPlaylistResponse,
-  GetPlaylistResponseType,
+  GetPlaylistTracksResponseType,
 } from '@spotify-playlist-manager/spotify-sdk';
 import { SpotifyWebApiClient } from '../plugins/spotify-web-api-client';
 import { HeadersContentTypeJsonType } from '../shared/schemas/content-type-schemas';
 import { BodySpotifyToken } from '../shared/schemas/spotify-token-schema';
+import { URL } from 'url';
 
 const PostPlaylistsExportBody = Type.Intersect([
   BodySpotifyToken,
@@ -25,7 +26,14 @@ const PostPlaylistsExportBody = Type.Intersect([
 export type PostPlaylistsExportBodyType = Static<typeof PostPlaylistsExportBody>;
 
 const PostPlaylistsExportResponse = {
-  200: Type.Array(GetPlaylistResponse),
+  200: Type.Array(
+    Type.Intersect([
+      Type.Omit(GetPlaylistResponse, ['tracks']),
+      Type.Object({
+        tracks: Type.Optional(GetPlaylistResponse.tracks),
+      }),
+    ]),
+  ),
 };
 export type PostPlaylistsExportResponseType = Static<
   typeof PostPlaylistsExportResponse['200']
@@ -35,6 +43,52 @@ export type PostPlaylistsExportResponseType = Static<
  * The server path prefix intended to be used with the router.
  */
 export const prefix = '/playlists';
+
+/**
+ * Load all tracks in a playlist.
+ *
+ * @param spotify the Axios client from the request, should have its own auth
+ * @playlistId ID of the playlist to fetch tracks for
+ * @param nextRequest the request URL to use in the next fetch
+ * @param accumulatedResponse accumulates all of the tracks over every response
+ * @returns the `accumulatedResponse` once Spotify indicates there are no more tracks
+ */
+const getAllPlaylistTracks = async (
+  spotify: AxiosInstance,
+  playlistId: string,
+  nextRequest:
+    | string
+    | null = `${spotify.defaults.baseURL}/playlists/${playlistId}/tracks`,
+  accumulatedResponse: GetPlaylistTracksResponseType = {
+    href: '',
+    items: [],
+    limit: 50,
+    next: null,
+    offset: 0,
+    previous: null,
+    total: 0,
+  },
+): Promise<GetPlaylistTracksResponseType> => {
+  if (nextRequest == null) {
+    return accumulatedResponse;
+  }
+
+  const url = new URL(nextRequest);
+  url.searchParams.set('limit', '50');
+  url.searchParams.sort();
+
+  const { data } = await spotify.get<GetPlaylistTracksResponseType>(url.toString());
+
+  const updatedAccumulation = {
+    ...accumulatedResponse,
+    items: [...accumulatedResponse.items, ...data.items],
+    next: data.next,
+    offset: data.offset,
+    total: data.total,
+  };
+
+  return getAllPlaylistTracks(spotify, playlistId, data.next, updatedAccumulation);
+};
 
 /**
  * A router plugin encapsulating all routes design for authentication and authorization to
@@ -58,30 +112,33 @@ export const router: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async (request, reply) => {
-      const { token, playlists } = request.body;
+      const { playlists } = request.body;
 
-      const meResponse = await request.webApi.get<GetMeResponseType>('/me');
+      const meResponse = await request.spotify.get<GetMeResponseType>('/me');
       const { id: userId } = meResponse.data;
 
       // hydrate the playlists
+      type HydratedPlaylist = PostPlaylistsExportResponseType[number];
       const hydratedPlaylists = await Promise.all(
         playlists.map(async ({ id, ownerId }) => {
-          const params = new URLSearchParams();
-          if (ownerId !== userId) {
-            // don't return tracks in playlists that the requesting user does not own
-            params.set('fields', '!tracks');
+          const userIsOwner = ownerId === userId;
+
+          let playlist: HydratedPlaylist;
+          ({ data: playlist } = await request.spotify.get<HydratedPlaylist>(
+            `/playlists/${id}`,
+            // don't immediately fetch tracks, we may not need them
+            { params: { fields: '(!tracks)' } },
+          ));
+
+          // if the user is the owner, then we'll fetch the tracks
+          if (userIsOwner && !playlist.error) {
+            playlist = {
+              ...playlist,
+              tracks: await getAllPlaylistTracks(request.spotify, id),
+            };
           }
 
-          const url = `/playlists/${id}`;
-
-          const playlistResponse = await request.webApi.get<GetPlaylistResponseType>(
-            url,
-            { params },
-          );
-
-          request.log.info({ playlistResponse });
-
-          return playlistResponse.data;
+          return playlist;
         }),
       );
 
@@ -89,5 +146,3 @@ export const router: FastifyPluginAsyncTypebox = async (fastify) => {
     },
   );
 };
-
-// todo need a "get playlist all tracks" function
--- packages/spotify-sdk/src/lib/web-api.ts
+++ packages/spotify-sdk/src/lib/web-api.ts
@@ -73,10 +73,12 @@ export const GetPlaylistResponse = Type.Intersect([
         external_urls: Type.Object({
           spotify: Type.String(),
         }),
-        followers: Type.Object({
-          href: Type.Union([Type.String(), Type.Null()]),
-          total: Type.Number(),
-        }),
+        followers: Type.Optional(
+          Type.Object({
+            href: Type.Union([Type.String(), Type.Null()]),
+            total: Type.Number(),
+          }),
+        ),
         href: Type.String(),
         id: Type.String(),
         type: Type.Literal('user'),
@@ -204,6 +206,126 @@ export const GetPlaylistResponse = Type.Intersect([
   }),
 ]);
 export type GetPlaylistResponseType = Static<typeof GetPlaylistResponse>;
+
+/**
+ * Response for `GET /playlist/{playlist_id}/tracks`
+ * https://developer.spotify.com/documentation/web-api/reference/#/operations/get-playlists-tracks
+ */
+export const GetPlaylistTracksResponse = Type.Intersect([
+  ResponseError,
+  Type.Object({
+    href: Type.String(),
+    /** Defined by the `GET /tracks/{track_id}` docs
+     * https://developer.spotify.com/documentation/web-api/reference/#/operations/get-track
+     */
+    items: Type.Array(
+      Type.Object({
+        added_at: Type.String(),
+        added_by: Type.Object({
+          external_urls: Type.Object({
+            spotify: Type.String(),
+          }),
+          href: Type.String(),
+          id: Type.String(),
+          type: Type.Literal('user'),
+          uri: Type.String(),
+        }),
+        is_local: Type.Boolean(),
+        primary_color: Type.Union([Type.Null()]),
+        track: Type.Object({
+          album: Type.Object({
+            album_type: Type.Union([
+              Type.Literal('album'),
+              Type.Literal('single'),
+              Type.Literal('compilation'),
+            ]),
+            artists: Type.Array(
+              Type.Object({
+                external_urls: Type.Object({
+                  spotify: Type.String(),
+                }),
+                href: Type.String(),
+                id: Type.String(),
+                type: Type.Literal('artist'),
+                uri: Type.String(),
+                name: Type.String(),
+              }),
+            ),
+            available_markets: Type.Array(Type.String()),
+            external_urls: Type.Object({
+              spotify: Type.String(),
+            }),
+            href: Type.String(),
+            id: Type.String(),
+            images: Type.Array(
+              Type.Object({
+                height: Type.Number(),
+                url: Type.String(),
+                width: Type.Number(),
+              }),
+            ),
+            name: Type.String(),
+            release_date: Type.String(),
+            release_date_precision: Type.Union([
+              Type.Literal('year'),
+              Type.Literal('month'),
+              Type.Literal('day'),
+            ]),
+            total_tracks: Type.Number(),
+            type: Type.Literal('album'),
+            uri: Type.String(),
+          }),
+          artists: Type.Array(
+            Type.Object({
+              external_urls: Type.Object({
+                spotify: Type.String(),
+              }),
+              href: Type.String(),
+              id: Type.String(),
+              type: Type.Literal('artist'),
+              uri: Type.String(),
+              name: Type.String(),
+            }),
+          ),
+          available_markets: Type.Array(Type.String()),
+          disc_number: Type.Number(),
+          duration_ms: Type.Number(),
+          episode: Type.Boolean(),
+          explicit: Type.Boolean(),
+          external_ids: Type.Partial(
+            Type.Object({
+              isrc: Type.String(),
+              ean: Type.String(),
+              upc: Type.String(),
+            }),
+          ),
+          external_urls: Type.Object({
+            spotify: Type.String(),
+          }),
+          href: Type.String(),
+          id: Type.String(),
+          is_local: Type.Boolean(),
+          name: Type.String(),
+          popularity: Type.Number(),
+          preview_url: Type.String(),
+          track: Type.Boolean(),
+          track_number: Type.Number(),
+          type: Type.Literal('track'),
+          uri: Type.String(),
+        }),
+        video_thumbnail: Type.Object({
+          url: Type.Union([Type.String(), Type.Null()]),
+        }),
+      }),
+    ),
+    limit: Type.Number(),
+    next: Type.Union([Type.String(), Type.Null()]),
+    offset: Type.Number(),
+    previous: Type.Union([Type.String(), Type.Null()]),
+    total: Type.Number(),
+  }),
+]);
+export type GetPlaylistTracksResponseType = Static<typeof GetPlaylistTracksResponse>;
 
 /**
  * Response for `GET /me/playlists`
