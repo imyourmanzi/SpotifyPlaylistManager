64b876039d8f5564e57dafd6a2d9f798cea05420
Matt Manzi <imyourmanzi1@gmail.com>
2022-11-09 22:00:12 -0500

    Let pino-pretty determine when to colorize

--- packages/server/src/environments/environment.ts
+++ packages/server/src/environments/environment.ts
@@ -8,7 +8,6 @@ export const environment: EnvironmentConfig = {
     transport: {
       target: 'pino-pretty',
       options: {
-        colorize: true,
         translateTime: 'HH:MM:ss Z',
         ignore: 'pid,hostname',
       },
