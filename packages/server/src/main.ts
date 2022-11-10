a9ba388e01fdc96dd97e96a969b44d0e7120f8e9
Matt Manzi <imyourmanzi1@gmail.com>
2022-11-09 21:12:19 -0500

    Don't need `server.close()`

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -3,6 +3,6 @@ import { server } from './server';
 server.listen({ port: 8888 }, (error) => {
   if (error) {
     server.log.error(error);
-    server.close().finally(() => process.exit(1));
+    process.exit(1);
   }
 });
